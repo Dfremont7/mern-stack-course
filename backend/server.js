a437b64927dd09adc31dfe346d58c10438ef252a
Dimitri FRÉMONT <dimitrifremont@Mac.lan>
2025-03-19 18:37:02 +0100

    reorganise route calls

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-import Product from './models/product.model.js';
+
+import productRoutes from './routes/product.route.js';
 
 dotenv.config();
 
@@ -9,33 +10,7 @@ const app = express();
 
 app.use(express.json()); // allow to accept JSON data in the req.body
 
-app.post('/api/products', async (req, res) => {
-  const product = req.body;
-
-  if (!product.name || !product.price || !product.image) {
-    return res.status(400).json({ success: false, message: 'Missing required fields' });
-  }
-
-  const newProduct = new Product(product);
-  try {
-    await newProduct.save();
-    res.status(201).json({ success: true, data: newProduct });
-  } catch (error) {
-    console.log('Error in Create product', error.message);
-    res.status(500).json({ success: false, message: "Server Error" });
-  }
-});
-
-app.delete('/api/products/:id', async (req, res) => {
-  const { id } = req.params;
-
-  try {
-    await Product.findByIdAndDelete(id);
-    res.status(200).json({ success: true, message: 'Product deleted successfully' });
-  } catch (error) {
-    res.status(400).json({ success: false, message: 'Error deleting product', error: error.message }); 
-  }  
-});
+app.use("/api/products", productRoutes);
 
 app.listen(5001, () => {
   connectDB();
