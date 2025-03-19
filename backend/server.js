119c5bc2708a5320e73b644592ef52edd16eeb03
Dimitri FRÉMONT <dimitrifremont@Mac.lan>
2025-03-19 17:36:28 +0100

    add delete request

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,17 @@ app.post('/api/products', async (req, res) => {
   }
 });
 
+app.delete('/api/products/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: 'Product deleted successfully' });
+  } catch (error) {
+    res.status(400).json({ success: false, message: 'Error deleting product', error: error.message }); 
+  }  
+});
+
 app.listen(5001, () => {
   connectDB();
   console.log('Server started at http://localhost:5001');
