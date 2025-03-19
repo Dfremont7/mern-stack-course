4a4ee54e7bcce2a07091059ab785246c918fe5f9
Dimitri FRÉMONT <dimitrifremont@Mac.lan>
2025-03-19 14:10:47 +0100

    import product model and add post request to test product creation

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,29 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import Product from './models/product.model.js';
 
 dotenv.config();
 
 const app = express();
 
-app.get('/products', (req, res) => {
-  res.send('Hello from the server!');
+app.use(express.json()); // allow to accept JSON data in the req.body
+
+app.post('/api/products', async (req, res) => {
+  const product = req.body;
+
+  if (!product.name || !product.price || !product.image) {
+    return res.status(400).json({ success: false, message: 'Missing required fields' });
+  }
+
+  const newProduct = new Product(product);
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.log('Error in Create product', error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
 });
 
 app.listen(5001, () => {
