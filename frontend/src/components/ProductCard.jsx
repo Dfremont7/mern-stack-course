c08ed4f0f2cae6fc7a55fc67d76fb5d3f6a84be0
Dimitri FRÉMONT <dimitrifremont@Mac.lan>
2025-03-31 19:50:28 +0200

    add image placeholder

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,6 +1,7 @@
 import { Box, Heading, HStack, IconButton, Image, Text, Toast } from "@chakra-ui/react"
 import { useColorModeValue, useToast } from "@chakra-ui/react";
 import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
+import ReactImageFallback from "react-image-fallback";
 
 import { useProductStore } from "../store/product.js";
 
@@ -32,13 +33,13 @@ const ProductCard = ({product}) => {
 
   return (
     <Box shadow={'lg'} rounded={'lg'} overflow={'hidden'} bg={bg}>
-      <Image src={product.image} alt={product.name} h={48} w='full' objectFit={'cover'}/>
+      <ReactImageFallback src={product.image} alt={product.name} minH={48} maxH={48} w={'100%'} objectFit='cover' fallbackImage="../../public/placeholder.svg"/>
       <Box p={4}>
         <Heading as={'h3'} size={'md'} mb={2}>
           {product.name}
         </Heading>
 
-        <Text color={TextColor} mb={4} fontWeight={'bold'}>
+        <Text color={TextColor} mb={4} fontWeight={'bold'} fontSize={'xl'}>
           ${product.price}
         </Text> 
 
