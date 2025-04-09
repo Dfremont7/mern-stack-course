d5183b5f936fa0496895b1fe8aefc00050396b9e
Dimitri FRÉMONT <dimitrifremont@Mac.lan>
2025-04-09 20:32:13 +0200

    reformat code

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,55 +1,75 @@
-import { Box, Heading, HStack, IconButton, Image, Text, Toast } from "@chakra-ui/react"
+import {
+  Box,
+  Heading,
+  HStack,
+  IconButton,
+  Image,
+  Text,
+  Toast,
+} from "@chakra-ui/react";
 import { useColorModeValue, useToast } from "@chakra-ui/react";
 import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
 import ReactImageFallback from "react-image-fallback";
 
 import { useProductStore } from "../store/product.js";
 
-const ProductCard = ({product}) => {
-  const TextColor = useColorModeValue('gray.600', 'gray.200');
-  const bg =  useColorModeValue('white', 'gray.800');
+const ProductCard = ({ product }) => {
+  const TextColor = useColorModeValue("gray.600", "gray.200");
+  const bg = useColorModeValue("white", "gray.800");
 
   const { deleteProduct } = useProductStore();
   const toast = useToast();
 
   const handleDelete = async (pid) => {
-    const {success, message} = await deleteProduct(pid);
+    const { success, message } = await deleteProduct(pid);
     if (!success) {
       toast({
-        title: 'Error',
+        title: "Error",
         description: message,
-        status: 'error',
-        isClosable: true
-      })
+        status: "error",
+        isClosable: true,
+      });
     } else {
       toast({
-        title: 'Success',
+        title: "Success",
         description: message,
-        status: 'success',
-        isClosable: true
-      })
+        status: "success",
+        isClosable: true,
+      });
     }
-  } 
+  };
 
   return (
-    <Box shadow={'lg'} rounded={'lg'} overflow={'hidden'} bg={bg}>
-      <ReactImageFallback src={product.image} alt={product.name} minH={48} maxH={48} w={'100%'} objectFit='cover' fallbackImage="../../public/placeholder.svg"/>
+    <Box shadow={"lg"} rounded={"lg"} overflow={"hidden"} bg={bg}>
+      <ReactImageFallback
+        src={product.image}
+        alt={product.name}
+        minH={48}
+        maxH={48}
+        w={"100%"}
+        objectFit="cover"
+        fallbackImage="../../public/placeholder.svg"
+      />
       <Box p={4}>
-        <Heading as={'h3'} size={'md'} mb={2}>
+        <Heading as={"h3"} size={"md"} mb={2}>
           {product.name}
         </Heading>
 
-        <Text color={TextColor} mb={4} fontWeight={'bold'} fontSize={'xl'}>
+        <Text color={TextColor} mb={4} fontWeight={"bold"} fontSize={"xl"}>
           ${product.price}
-        </Text> 
+        </Text>
 
         <HStack spacing={2}>
           <IconButton icon={<EditIcon />} colorScheme="blue" />
-          <IconButton icon={<DeleteIcon />} colorScheme="red" onClick={() => handleDelete(product._id)} />
+          <IconButton
+            icon={<DeleteIcon />}
+            colorScheme="red"
+            onClick={() => handleDelete(product._id)}
+          />
         </HStack>
       </Box>
     </Box>
-  )
-}
+  );
+};
 
 export default ProductCard;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
-import React, { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { ChakraProvider } from '@chakra-ui/react'
-import { BrowserRouter } from 'react-router-dom'
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
       <BrowserRouter>
@@ -12,4 +12,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
-)
+);
